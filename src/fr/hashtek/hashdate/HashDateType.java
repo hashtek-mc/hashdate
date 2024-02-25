21d63903ccf3dc83cb27faa080a3aafaa2ebcfe5
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-02-25 16:32:13 +0100

    [FIX] Fixed a bug where minutes and months were inverted (in SHORT).

--- src/fr/hashtek/hashdate/HashDateType.java
+++ src/fr/hashtek/hashdate/HashDateType.java
@@ -5,7 +5,7 @@ public enum HashDateType {
     /* Example date: 05/01/2021 14:17:42 */
 
     FANCY("EEEE d MMMM yyyy 'à' HH:mm:ss"), // Mardi 05 février 2021 à 14:17:42
-    SHORT("dd/mm/yyyy HH:mm:ss");           // 05/01/2021 14:17:42
+    SHORT("dd/MM/yyyy HH:mm:ss");           // 05/01/2021 14:17:42
 
 
     private final String format;
