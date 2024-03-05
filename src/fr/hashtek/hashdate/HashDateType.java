48d31b45061e382a03710d2593d2bbc5f1f66636
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-03-05 03:55:20 +0100

    [UPDATE] Made the code more clean

--- src/fr/hashtek/hashdate/HashDateType.java
+++ src/fr/hashtek/hashdate/HashDateType.java
@@ -4,8 +4,8 @@ public enum HashDateType {
 
     /* Example date: 05/01/2021 14:17:42 */
 
-    FANCY("EEEE d MMMM yyyy 'à' HH:mm:ss"), // Mardi 05 février 2021 à 14:17:42
-    SHORT("dd/MM/yyyy HH:mm:ss");           // 05/01/2021 14:17:42
+    FANCY ("EEEE d MMMM yyyy 'à' HH:mm:ss"), // Mardi 05 février 2021 à 14:17:42
+    SHORT ("dd/MM/yyyy HH:mm:ss");           // 05/01/2021 14:17:42
 
 
     private final String format;
