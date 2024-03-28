6d3befe8b9c9819ffd629c9dd5585121619251ec
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-03-28 22:22:08 +0100

    feat(datetype): Added some types + fixed some coding style issues.

--- src/fr/hashtek/hashdate/HashDateType.java
+++ src/fr/hashtek/hashdate/HashDateType.java
@@ -1,11 +1,15 @@
 package fr.hashtek.hashdate;
 
-public enum HashDateType {
+public enum HashDateType
+{
 
     /* Example date: 05/01/2021 14:17:42 */
 
-    FANCY ("EEEE d MMMM yyyy 'à' HH:mm:ss"), // Mardi 05 février 2021 à 14:17:42
-    SHORT ("dd/MM/yyyy HH:mm:ss");           // 05/01/2021 14:17:42
+    FANCY_DATE_TIME ("EEEE d MMMM yyyy 'à' HH:mm:ss"),  // Mardi 05 février 2021 à 14:17:42
+    SHORT_DATE_TIME ("dd/MM/yyyy HH:mm:ss"),            // 05/01/2021 14:17:42
+    FANCY_DATE      ("EEEE d MMMM yyyy"),               // Mardi 05 février 2021
+    SHORT_DATE      ("dd/MM/yyyy"),                     // 05/01/2021
+    TIME            ("HH:mm:ss");                       // 14:17:42
 
 
     private final String format;
